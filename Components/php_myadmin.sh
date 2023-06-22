1bbdf35d0147614eb853e6cd132020201c53e065
Maks Grupiński <maks.grupinski@solve.md>
2023-06-22 11:43:22 +0200

    Fixes

--- Components/php_myadmin.sh
+++ Components/php_myadmin.sh
@@ -5,12 +5,13 @@ echo "phpmyadmin phpmyadmin/app-password-confirm password ${PHP_PASSWD}" | sudo 
 echo "phpmyadmin phpmyadmin/mysql/admin-pass password ${MYSQL_PASSWD}" | sudo debconf-set-selections
 echo "phpmyadmin phpmyadmin/mysql/app-pass password ${MYSQL_PASSWD}" | sudo debconf-set-selections
 
-sudo apt install -y phpmyadmin php-mbstring php-gettext
+sudo apt install -y phpmyadmin php8.1-mbstring php-php-gettext
 
-sudo phpenmod mbstring
+sudo phpenmod -v 8.1 mbstring
 
 sudo systemctl restart apache2
 
 curl -fsS "http://localhost/phpmyadmin" > /dev/null
 if [ $? -eq 0 ]; then
 	echo "phpMyAdmin works"
+fi
