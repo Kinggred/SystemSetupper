9be239a474ffd3b7c69cf2b34380cc81c5a445a4
Maks Grupiński <maks.grupinski@solve.md>
2023-06-22 11:34:34 +0200

    Added phpmyadmin

---
+++ Components/php_myadmin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "phpmydamin phpmyadmin/dbconfig-install boolean true" | sudo debconf-set-selections
+echo "phpmyadmin phpmyadmin/app-password-confirm password ${PHP_PASSWD}" | sudo debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/admin-pass password ${MYSQL_PASSWD}" | sudo debconf-set-selections
+echo "phpmyadmin phpmyadmin/mysql/app-pass password ${MYSQL_PASSWD}" | sudo debconf-set-selections
+
+sudo apt install -y phpmyadmin php-mbstring php-gettext
+
+sudo phpenmod mbstring
+
+sudo systemctl restart apache2
+
+curl -fsS "http://localhost/phpmyadmin" > /dev/null
+if [ $? -eq 0 ]; then
+	echo "phpMyAdmin works"
