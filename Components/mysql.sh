596c7957eb4d243042a4ccca6a48d4488253f20a
Maks Grupiński <maks.grupinski@solve.md>
2023-06-22 14:47:17 +0200

    Fixes

--- Components/mysql.sh
+++ Components/mysql.sh
@@ -8,3 +8,7 @@ sudo apt install -y mysql-server
 sudo service mysql start
 
 sudo service mysql status
+
+sudo mysql -uroot -p -e "CREATE USER '${USERNAME}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWD}';"
+sudo mysql -uroot -p -e "GRANT ALL PRIVILEGES ON *.* TO '${USERNAME}'@'localhost' WITH GRANT OPTION;"
+sudo mysql -uroot -p -e "FLUSH PRIVILEGES;"
