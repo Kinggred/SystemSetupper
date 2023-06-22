4918629948bdb3b6c88605917d489848d8f80f7f
Maks Grupiński <maks.grupinski@solve.md>
2023-06-22 13:56:10 +0200

    Fixes

--- Components/mysql.sh
+++ Components/mysql.sh
@@ -4,3 +4,7 @@ echo "mysql-server mysql-server/root_password password ${MYSQL_PASSWD}"
 echo "mysql-server mysql-server/root_password_again password ${MYSQL_PASSWD}"
 
 sudo apt install -y mysql-server
+
+sudo service mysql start
+
+sudo service mysql status
