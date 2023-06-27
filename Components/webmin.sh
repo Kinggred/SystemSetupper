e1adb6e354a1365604adbe132298f29a7b365704
Maks Grupiński <maks.grupinski@solve.md>
2023-06-27 19:00:17 +0200

    Changed permissions

--- Components/webmin.sh
+++ Components/webmin.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 sudo echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
-wget -qO - http://www.webmin.com/jcameron-key.asc | sudo apt-key add -
+sudo apt install gnupg -y
+curl -s http://www.webmin.com/jcameron-key.asc | sudo gpg --dearmor --yes -o /usr/share/keyrings/webmin.gpg
+echo "deb [signed-by=/usr/share/keyrings/webmin.gpg] http://download.webmin.com/download/repository sarge contrib" | sudo tee /etc/apt/sources.list.d/webmin.list
 
+sudo apt-get update -y
 sudo apt-get install webmin -y
 
+# Restart the server
 sudo service apache2 restart
